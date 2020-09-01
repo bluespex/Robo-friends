ca8550dca44ba622835bafa58fe75ac9433a386f
Piyush <44617356+piyush-shandilya@users.noreply.github.com>
2020-09-01 16:48:08 +0530

    Updates

---
+++ precache-manifest.ab2c785c3c28bd1309c427046e9eeb73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d175cc3c3725d4a0e62515c89ba7892",
+    "url": "/bluespex/Robo-friends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/bluespex/Robo-friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d3831322c85858c2dbe6",
+    "url": "/bluespex/Robo-friends/static/css/main.9d1baace.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/bluespex/Robo-friends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "d3831322c85858c2dbe6",
+    "url": "/bluespex/Robo-friends/static/js/main.23fb3e1b.chunk.js"
+  },
+  {
+    "revision": "2a9e1dd349c94fc2e68f",
+    "url": "/bluespex/Robo-friends/static/js/runtime~main.2dbff653.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/bluespex/Robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
